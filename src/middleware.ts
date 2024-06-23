5f7666108ea1be30780b6deb52470d452241b8ab
Marcos Essindi <marcessindi@me.com>
2024-06-23 13:32:11 +0200

    Remove log

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,7 @@ import type { NextRequest } from "next/server";
 import { getSession } from "./app/session/session";
 
 export async function middleware(request: NextRequest) {
-	console.log("get session");
 	const session = await getSession();
-	console.log("session", session);
 
 	if (!session.userId && !request.nextUrl.pathname.startsWith("/login")) {
 		return Response.redirect(new URL("/login", request.url));
