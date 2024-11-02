3001bb37471eaae07fc8f91484eae9f7371cb093
Marcos Essindi <marcessindi@me.com>
2024-11-02 09:24:43 +0100

    Add loading.tsx to boards/[id]

---
+++ app/boards/[id]/loading.tsx
@@ -0,0 +1,5 @@
+import { BoardFallBack } from "./_components/board-fallback";
+
+export default function loading() {
+	return <BoardFallBack />;
+}
--- app/boards/[id]/page.tsx
+++ app/boards/[id]/page.tsx
@@ -1,15 +1,9 @@
 import { ensureUser } from "@lib/user";
-import { Suspense } from "react";
 import { Board } from "./_components/board";
-import { BoardFallBack } from "./_components/board-fallback";
 import { boardData } from "./_lib/queries";
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
 	const { id } = await params;
 	const user = await ensureUser();
-	return (
-		<Suspense fallback={<BoardFallBack />}>
-			<Board board={boardData(parseInt(id), user.id)} />
-		</Suspense>
-	);
+	return <Board board={boardData(parseInt(id), user.id)} />;
 }
