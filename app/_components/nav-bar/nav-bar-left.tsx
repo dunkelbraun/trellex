de0a68a48f0d950c8190594bebe8b6beb1792a2f
Marcos Essindi <marcessindi@me.com>
2024-11-11 09:06:28 +0100

    Prefetch link

--- app/_components/nav-bar/nav-bar-left.tsx
+++ app/_components/nav-bar/nav-bar-left.tsx
@@ -6,7 +6,7 @@ import { Text, View } from "reshaped";
 export function NavBarLeft({ isLoggedIn }: { isLoggedIn: boolean }) {
 	return (
 		<View direction="row" gap={2}>
-			<Link href={isLoggedIn ? "/boards" : "/"}>
+			<Link href={isLoggedIn ? "/boards" : "/"} prefetch={true}>
 				<Text variant="body-3" weight="bold">
 					Trellex
 				</Text>
